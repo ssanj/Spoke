c5534f50e03f063825a6a6df6411237df0730328
sanjiv sahayam <sanjsmailbox@gmail.com>
2014-05-15 17:02:12 +1000

    1. Added ParseResult type to encode parsing result 2. Added workaround to URL to handle special characters like in font.google.com

--- src/main/scala/spoke/Elements.scala
+++ src/main/scala/spoke/Elements.scala
@@ -6,42 +6,31 @@ import org.htmlcleaner.{TagNode, HtmlCleaner}
 /*
  *
  *  1. remove dupes
- *  2. Skipped and InError should be part of another Type.
  *
  */
 
 object Elements {
 
-  sealed abstract class HtmlElement {
-    def isInError = false
-    def isSkipped = false
-  }
-
+  sealed abstract class HtmlElement
   case class Anchor(name:Option[String], link:String) extends HtmlElement
-
   case class Stylesheet(link:String) extends HtmlElement
-
   case class Script(link:String) extends HtmlElement
-
   case class Image(name:Option[String], link:String) extends HtmlElement
 
-  //TODO:Skipped and InError are not really HtmlElements and should model the Success or Failure of a parse instead.
-  case class Skipped(reason:String) extends HtmlElement {
-    override def isSkipped = true
-  }
+  abstract sealed class ParseResult
+  case class Parsed(element:HtmlElement) extends ParseResult
+  case class Skipped(reason:String, tagNode:TagNode) extends ParseResult
+  case class Error(reason:String, tagNode:TagNode) extends ParseResult
 
-  case class InError(reason:String) extends HtmlElement {
-    override def isInError = true
-  }
-
-  case class ElementSummary(valid:Seq[HtmlElement], skipped:Seq[HtmlElement], failed:Seq[HtmlElement])
+  case class FailureSummary(reason:String, tagNode:TagNode)
+  case class ElementSummary(valid:Seq[HtmlElement], skipped:Seq[FailureSummary], failed:Seq[FailureSummary])
 
   val httpUrl = "^https?://.*"
   val protocoledUrl = "^.*:.*"
   val httpUnspecifiedUrl = "^//.*"
 
   def getElementSummary(url:String): ElementSummary = {
-    val elements = getElementByType(getRootNode(url), Seq("a", "link", "img", "script")).map(nodeToElement(url))
+    val elements:Seq[ParseResult] = getElementByType(getRootNode(url), Seq("a", "link", "img", "script")).map(nodeToElement(url))
     parseHtml(elements)
   }
 
@@ -49,17 +38,18 @@ object Elements {
     if (f(value)) Some(value) else None
   }
 
-  private def parseHtml(htmlElements:Seq[HtmlElement]): ElementSummary = {
-    val (success, error) = htmlElements.partition(!_.isInError)
-    val (valid, skipped) = success.partition(!_.isSkipped)
-    ElementSummary(valid, skipped, error)
+  private def parseHtml(parsedResults:Seq[ParseResult]): ElementSummary = {
+    ElementSummary(parsedResults.collect { case Parsed(el) => el },
+                   parsedResults.collect { case Skipped(r, tn) => FailureSummary(r, tn) },
+                   parsedResults.collect { case Error(r, tn) => FailureSummary(r, tn) }
+    )
   }
 
   private def getElementByType(root:TagNode, elementTypes:Seq[String]):Seq[TagNode] = {
     elementTypes.flatMap(e => root.getElementsByName(e, true).toSeq)
   }
 
-  private def nodeToElement(url:String)(tagNode:TagNode):HtmlElement = {
+  private def nodeToElement(url:String)(tagNode:TagNode):ParseResult = {
     import scala.collection.convert.Wrappers._
 
     implicit val attribs = JMapWrapper(tagNode.getAttributes).toMap
@@ -67,17 +57,19 @@ object Elements {
     val domain= getDomain(url)
 
     tagNode.getName match {
-      case "a" => createHtmlElement("href", wrapUrl(domain, href => {
-        if (!href.matches(httpUrl)) Skipped("non-http(s): anchor link [%s -> %s]".format(tagNode.getText, href))
-        else Anchor(booleanToOption[String](!_.isEmpty, tagNode.getText.toString.trim), href)
-      }), "Could not find href for anchor tag: [$tagName]")
+      case "a" => createHtmlElement("href", wrapUrl(domain, createAnchor(tagNode)), "Could not find href for anchor tag: [$tagName]", Error(_, tagNode))
 
-      case "link" => createHtmlElement("href", wrapUrl(domain, Stylesheet), s"Could not find href for link tag: [$tagNode]")
+      case "link" => createHtmlElement("href", wrapUrl(domain, s => Parsed(Stylesheet(s))), s"Could not find href for link tag: [$tagNode]", Error(_, tagNode))
 
-      case "script" => createHtmlElement("src", wrapUrl(domain, Script), "Could not find src for script tag", Skipped)
+      case "script" => createHtmlElement("src", wrapUrl(domain, s => Parsed(Script(s))), "Could not find src for script tag", Skipped(_, tagNode))
 
-      case "img" => createHtmlElement("src", wrapUrl(domain, Image(attribs.get("alt"), _)), s"Could not find src for Img tag: [$tagNode]")
+      case "img" => createHtmlElement("src", wrapUrl(domain, s => Parsed(Image(attribs.get("alt"), s))), s"Could not find src for Img tag: [$tagNode]", Error(_, tagNode))
     }
+  }
+
+  private def createAnchor(tagNode:TagNode)(href:String): ParseResult = {
+    if (!href.matches(httpUrl)) Skipped("non-http(s): anchor link [%s -> %s]".format(tagNode.getText, href), tagNode)
+    else Parsed(Anchor(booleanToOption[String](!_.isEmpty, tagNode.getText.toString.trim), href))
   }
 
   private def getDomain(url:String) = {
@@ -85,7 +77,7 @@ object Elements {
     s"${u.getProtocol}://${u.getHost}${ if (u.getPort == -1) "" else u.getPort }"
   }
 
-  private def wrapUrl(domain:String, block:(String) => HtmlElement) = (link:String) => block(getAbsoluteUrl(domain, link))
+  private def wrapUrl(domain:String, block:(String) => ParseResult) = (link:String) => block(getAbsoluteUrl(domain, link))
 
   private def getAbsoluteUrl(domain:String, link:String): String = {
 
@@ -97,7 +89,7 @@ object Elements {
       } else link //already absolute or not http(s).
   }
 
-  private def createHtmlElement(key:String, block:(String) => HtmlElement, reason:String, errorHandler:String => HtmlElement = InError)(implicit attributes:Map[String, String]): HtmlElement = {
+  private def createHtmlElement(key:String, block:(String) => ParseResult, reason:String, errorHandler:String => ParseResult)(implicit attributes:Map[String, String]): ParseResult = {
     attributes.get(key).map { k =>
       block(k)
     }.getOrElse(errorHandler(reason))
--- src/main/scala/spoke/SpokeRunner.scala
+++ src/main/scala/spoke/SpokeRunner.scala
@@ -4,7 +4,7 @@ import com.stackmob.newman.ApacheHttpClient
 import scala.concurrent.{ExecutionContext, Future}
 import com.stackmob.newman.response.HttpResponseCode
 import com.stackmob.newman.dsl._
-import java.net.URL
+import java.net.{URI, URL}
 import com.stackmob.newman.response.HttpResponseCode.Ok
 import spoke.Elements._
 import spoke.report.BasicReport
@@ -18,21 +18,24 @@ object SpokeRunner extends App with Spoke {
 
   def createRequest(url:String):Future[HttpResponseCode] =
     try {
-      HEAD(new URL(url)).apply.map(r =>  r.code)
+      HEAD(encodeURL(url)).apply.map(r =>  r.code)
     } catch {
       case x:Throwable => promise[HttpResponseCode]().failure(x).future
     }
 
-  //TODO:Make this Option[Future[(String,HttpResponseCode)]
   def getFuture(element:HtmlElement):Future[HttpResponseCode] = element match {
     case Anchor(_, link) => createRequest(link)
     case Stylesheet(link) => createRequest(link)
     case Script(link) => createRequest(link)
     case Image(_, link:String) => createRequest(link)
-    case x => sys.error("Invalid request type: %s".format(x))
   }
 
-//  val urls = Seq("http://iys.org.au", "http://iys.org.au/events.html", "http://iys.org.au/contact.html")
+  private def encodeURL(url:String): URL = {
+    val u = new URL(url)
+    //create a URI from the URL to properly encode any special characters. The extract a new URL
+    new URI(u.getProtocol, u.getHost, u.getPath, u.getQuery, null).toURL
+  }
+
   val urls = Seq("http://www.baysidetidybags.com.au/", "http://www.baysidetidybags.com.au/services/", "http://www.baysidetidybags.com.au/contact-us/")
   BasicReport.printWithErrors(checks[HttpResponseCode](urls, getFuture, _ == Ok))
 }
